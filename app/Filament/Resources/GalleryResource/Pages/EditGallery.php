710b4a4987e6b8c0d63cdf5174dd492a5540b0a7
Lexh19 <iqbaalhzd@gmail.com>
2024-08-05 09:24:39 +0700

    home,about and servis

--- app/Filament/Resources/GalleryResource/Pages/EditGallery.php
+++ app/Filament/Resources/GalleryResource/Pages/EditGallery.php
@@ -2,9 +2,11 @@
 
 namespace App\Filament\Resources\GalleryResource\Pages;
 
-use App\Filament\Resources\GalleryResource;
 use Filament\Actions;
+use App\Models\gallery;
+use Illuminate\Support\Facades\Storage;
 use Filament\Resources\Pages\EditRecord;
+use App\Filament\Resources\GalleryResource;
 
 class EditGallery extends EditRecord
 {
@@ -13,7 +15,13 @@ class EditGallery extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make() ->after(
+                function (gallery $record) {
+                    if ($record->img) {
+                        Storage::disk('public')->delete($record->img);
+                    }
+                }
+            ),
         ];
     }
 }
--- app/Models/gallery.php
+++ app/Models/gallery.php
@@ -2,11 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class gallery extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'img'];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::updating(function ($model) {
+            if ($model->isDirty('img') && ($model->getOriginal('img') !== null)) {
+                Storage::disk('public')->delete($model->getOriginal('img'));
+            }
+        });
+    }
 }
