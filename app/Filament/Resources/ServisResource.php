a43ab33fae4763196cda53de24a72bdda9b59ddc
nectargg27 <80022241+nectargg27@users.noreply.github.com>
2024-08-08 16:24:22 +0700

    Update ServisResource.php

--- app/Filament/Resources/ServisResource.php
+++ app/Filament/Resources/ServisResource.php
@@ -19,7 +19,7 @@ class ServisResource extends Resource
 {
     protected static ?string $model = Servis::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-clipboard-document-list';
 
     public static function form(Form $form): Form
     {
